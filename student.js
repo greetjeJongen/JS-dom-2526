2d77b1e5fa1e0591424ca8ff12d2686d5c3df308
greetjeJongen <greetje.jongen@ucll.be>
2025-11-06 11:09:50 +0100

    first commit

---
+++ student.js
@@ -0,0 +1,8 @@
+// Oefening 1: Voetbal uitslagen 
+
+
+
+// Oefening 2: Getallenvierkant
+const zijde = 14; // kan je later aanpassen om grotere of kleinere vierkanten te tekenen
+
+// schrijf hieronder je code  
